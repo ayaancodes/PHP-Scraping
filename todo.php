99fedac599db73c3cef7906c0411c688dd60f051
Ayaan Munshi <ayaanmunshi@Ayaans-MacBook-Pro.local>
2021-06-30 01:33:15 -0400

    need review from russ

--- todo.php
+++ todo.php
@@ -16,7 +16,16 @@ session_start();
 			mysqli_query($db1, $sql);
 			header('location: todo.php');
 		}
-	}	
+	}
+	// delete task
+	if (isset($_GET['del_task'])) {
+		$id = $_GET['del_task'];
+
+		mysqli_query($db1, "DELETE FROM tasks WHERE id=".$id);
+		header('location: index.php');
+	}
+
+?>
 
 ?>
 
@@ -34,6 +43,31 @@ session_start();
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
 	</form>
+	<table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($db1, "SELECT * FROM tasks") or die( mysqli_error($db1));
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+	</table>
 	<?php if (isset($errors)) { ?>
 	<p><?php echo $errors; ?></p>
 	<?php } ?>
