ca120a5447870587e551ab5b7531308ec79acfd8
Ayaan Munshi <ayaanmunshi2004@gmail.com>
2021-06-30 00:48:48 -0400

    fixing bugs

--- scraper.php
+++ scraper.php
@@ -4,7 +4,7 @@
 
 
 
-=======
+
 
  //base url
 $base = 'https://apps.npr.org/dailygraphics/graphics/coronavirus-d3-world-map-20200323/table.html?initialWidth=1238&childId=responsive-embed-coronavirus-d3-world-map-20200323-table&parentTitle=Coronavirus%20World%20Map%3A%20Tracking%20The%20Spread%20Of%20The%20Outbreak%20%3A%20Goats%20and%20Soda%20%3A%20NPR&parentUrl=https%3A%2F%2Fwww.npr.org%2Fsections%2Fgoatsandsoda%2F2020%2F03%2F30%2F822491838%2Fcoronavirus-world-map-tracking-the-spread-of-the-outbreak';
@@ -38,7 +38,7 @@ foreach($html_base->find('div') as $element) {
 $html_base->clear(); 
 unset($html_base);
 
-=======
+
 
 // Load HTML from a string
 $html_base->load($str);
