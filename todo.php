7ea56000a4d0b828c354e52e648c023d548d8ec4
russ mehta <russmehta01@gmail.com>
2021-07-02 16:15:03 -0400

    added new table tasks in the login db and linked it to the users_id so users can view and delete their tasks only.

--- todo.php
+++ todo.php
@@ -1,10 +1,20 @@
 <?php 
+
+include("functions.php");
+
 session_start();
+
+
     // initialize errors variable
 	$errors = "";
 
 	// connect to database
-	$db1 = mysqli_connect("localhost", "root", "", "todo");
+	$db1 = mysqli_connect("localhost", "root", "", "login_sample_db");
+
+	$user_data = check_login($db1);
+
+	$currentId = $user_data['id'];
+
 
 	// insert a quote if submit button is clicked
 	if (isset($_POST['submit'])) {
@@ -12,11 +22,17 @@ session_start();
 			$errors = "You must fill in the task";
 		}else{
 			$task = $_POST['task'];
-			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			$sql = "INSERT INTO tasks (task , users_id) VALUES ('$task' , '$currentId')";
 			mysqli_query($db1, $sql);
 			header('location: todo.php');
 		}
-	}	
+	}
+	// delete task
+	if (isset($_GET['del_task'])) {
+		$id = $_GET['del_task'];
+		mysqli_query($db1, "DELETE FROM tasks WHERE users_id=$currentId AND id=$id");
+		// header('location: index.php');
+	}
 
 ?>
 
@@ -34,6 +50,33 @@ session_start();
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
 	</form>
+	<table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Task id</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($db1, "SELECT * FROM tasks WHERE users_id=$currentId") or die( mysqli_error($db1));
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="taskid"> <?php echo $row['id']; ?> </td>
+				<td class="task"><?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="todo.php?del_task=<?php echo $row['id']?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+	</table>
 	<?php if (isset($errors)) { ?>
 	<p><?php echo $errors; ?></p>
 	<?php } ?>
@@ -43,4 +86,3 @@ session_start();
 <a href="changepass.php">Click here</a>
 </body>
 </html>
-
